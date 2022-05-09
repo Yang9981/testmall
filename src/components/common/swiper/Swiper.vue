0ed1433cc8cafa0bfb49421c27452e68ac48b86c
Yang <1563040349@qq.com>
2022-05-09 16:26:46 +0800

    轮播图

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide" :v-for="item in banners">
-                {{item}}
+            <div class="swiper-slide" :v-for="item in banners" ：key='item'>
+                <img :src="item.image" alt="">
             </div>
         </div>
         <div class="swiper-button-back"></div>
@@ -19,17 +19,21 @@ import "swiper/dist/css/swiper.min.css";
 export default {
     name:'swiper',
     props:['banners'],
+    /* 轮播图的使用 */
     mounnted(){
         var mySwiper=new Swiper(".swiper-container",{
             loop:true,
+            /* 自动播放 */
             autoplay:{
                 delay:2000,
                 disableOnInteraction:false
             },
+            /* 小点 */
             pagination:{
                 el:'.swiper-pagination',
                 clickable:true
             },
+            /* 左右按钮 */
             navigation:{
                 nextEl:'.swiper-button-next',
                 prveEl:'.swiper-button-prve'
